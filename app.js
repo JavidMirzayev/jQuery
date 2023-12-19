a14499cb05ee1d63c836c694e5e1eeeaa8fa06ce
Cavid Mirzeyev <javid.mirzeyev@gmail.com>
2023-12-19 13:24:45 +0000

    html

--- app.js
+++ app.js
@@ -32,22 +32,7 @@ $(document).ready(function () {
 
 
 
-    $("button").click(() => {
-        let inputValue = $("input").val();
-        let numericValue = Number(inputValue);
-        
-        inputValue == "" ? console.log("Zəhmət olmasa boş buraxmayın!") : !numericValue
-            ? $(".list").append($("<h2>").text(inputValue)) : numericValue && appendNumericTodos(numericValue);
-
-        $("input").val("");
-
-        function appendNumericTodos(value) {
-            for (let i = 0; i < value; i++) {
-                let numericTodos = $("<h2>").text(value);
-                $(".list").append(numericTodos);
-            }
-        }
-    });
+   
 
 });
 
