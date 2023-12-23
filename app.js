95f45ace9c85998ddd6b3cfb42a9227567ce9665
Cavid Mirzeyev <javid.mirzeyev@gmail.com>
2023-12-23 13:06:31 +0000

    jquery

--- app.js
+++ app.js
@@ -2,12 +2,32 @@
 $(document).ready(function () {
 
 
+    $('.btn').click(function (e) {
+        e.preventDefault();
+        function myel(myclass = "") {
+            if (!$.trim($("#inputme").val()) == '') {
+                let valle = $("#inputme").val()
+                let newDiv = $('<h2></h2>')
+                let bigDiv = $(`<div ${myclass}></div>`)
+                $(newDiv).append(valle)
+                $(bigDiv).append(newDiv)
+                return bigDiv, newDiv
+
+            }
+            else {
+
+            }
+        }
+        $("#listme").append(myel())
 
 
 
+    })
+
+  
 
 
-   
+
 
 
 
